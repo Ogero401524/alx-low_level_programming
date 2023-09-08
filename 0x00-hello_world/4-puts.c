86f062b3d2f7676b9711bcb0af15fadc5fe98684
Ogero401524 <amirakraphael@gmail.com>
2023-09-08 08:41:07 -0700

    first c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  *main - Entry
  *
- *Return always (0)
+ *Return: always 0 (success)
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
