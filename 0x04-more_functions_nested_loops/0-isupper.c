6bca837fb58f8af85b88ed6ae7a0758377114be5
Ogero401524 <amirakraphael@gmail.com>
2023-09-15 05:57:20 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - entry
+ *checks for uppercase character
+ *Returns 1 if c is uppercase, zero otherwise
+ */
+int main(void)
+{
+	int _isupper(int c)
+	{
+		if (c = 'A' <= 'Z')
+		{
+			return (1);
+		}
+		else
+			return (0);
+	}
+}
