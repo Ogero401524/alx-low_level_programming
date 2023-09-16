4edbda54c1cc459e16f339517d2f37af114315d0
Ogero401524 <amirakraphael@gmail.com>
2023-09-15 19:30:54 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_line - give n times
+ *always: return 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			break;
+		}
+		_putchar(_);
+	}
+	_putchar('\n');
+}
