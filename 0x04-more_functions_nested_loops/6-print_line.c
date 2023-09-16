0d379bdad58f76e6b8e7aa359f381b37502332fb
Ogero401524 <amirakraphael@gmail.com>
2023-09-15 19:46:08 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,15 +5,16 @@
  */
 void print_line(int n)
 {
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+
 	int i;
 
 	for (i = 0; i <= n; i++)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
 		_putchar('_');
 	}
 	_putchar('\n');
