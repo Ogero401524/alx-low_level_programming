9e48d3e61002a8aad213b0af29cdd11b123f593e
Ogero401524 <amirakraphael@gmail.com>
2023-10-04 06:24:27 -0700

    corresct malloc dechkeration

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	_array = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
+	if (size == 0 || _array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
