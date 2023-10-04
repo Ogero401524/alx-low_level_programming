37482991d61c7270b8fb2923fa594eded4e4698b
Ogero401524 <amirakraphael@gmail.com>
2023-10-04 08:11:22 -0700

    strup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,6 +14,8 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
 	copy = malloc(sizeof(char) * (count + 1));
 
 	if (copy == NULL)
@@ -21,7 +23,6 @@ char *_strdup(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		count++;
 		copy[i] = str[i];
 	}
 
