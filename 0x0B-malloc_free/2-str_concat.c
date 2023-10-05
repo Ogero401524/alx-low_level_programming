843d8ae7db84df1efb0638956a4fce3926d6f78d
Ogero401524 <amirakraphael@gmail.com>
2023-10-04 17:57:10 -0700

    concat while loop stax

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 /**
  * str_concat - Entry point
-* @s2: input two to concat
+ *@s1: string
+* @s2: string
  * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
@@ -24,6 +25,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (concat == NULL)
 		return (NULL);
+	i = j = 0;
 	while (s1[i] != '\0')
 	{
 		concat[i] = s1[i];
