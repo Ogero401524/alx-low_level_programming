cfecdb4b590e2ffd4764681f53b63b6cc91e04dd
Ogero401524 <amirakraphael@gmail.com>
2023-10-10 08:50:30 -0700

    typedef pleced correctly

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef MAIN_STRUCT
 #define MAIN_STRUCT
-typedef struct dog dog_t;
+/**
+ * struct dog - A structure to represent dog information.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 #endif
