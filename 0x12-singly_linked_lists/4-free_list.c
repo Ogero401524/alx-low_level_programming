fdfddd71268598ffffd23d102d661a5bddac257a
Ogero401524 <amirakraphael@gmail.com>
2023-10-19 19:56:00 -0700

    gree meme

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+* free_list - Frees a list_t linked list
+* @head: A pointer to the head of the linked list
+*/
+void free_list(list_t *head)
+{
+    list_t *current_pos, *temp;
+
+    while (head)
+    {
+        current_pos = head;
+        temp = current_pos->next;
+
+        free(current_pos->str);
+        free(current_pos);
+
+        head = temp;
+    }
+}
