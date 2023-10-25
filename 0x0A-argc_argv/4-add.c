933616bac4ffe4fa47b7a5a53105030518731fb6
Ogero401524 <amirakraphael@gmail.com>
2023-10-25 08:02:15 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,60 +1,35 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * custom_atoi - Convert a string to an integer.
- * @str: The string to convert.
- * Return: The converted integer.
- */
-int _atoi(const char *str) {
-    int num = 0;
-    if (*str == '\0')
-	    return 0;
-    while (*str) {
-        num = num * 10 + (*str - '0');
-        str++;
-    }
-    return num;
-}
+#include <stdlib.h>
 
 /**
- * check_positive_number - Check if a string is a positive number.
- * @str: The string to check.
- * Return: 1 if it's a positive number, 0 otherwise.
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 on success, 1 on error
  */
-int check_positive_number(const char *str) {
-    if (*str == '\0')
-        return 0;
+int main(int argc, char *argv[])
+{
+    int i, j, sum = 0;
 
-    while (*str)
+    if (argc < 2)
     {
-        if (*str < '0' || *str > '9')
-            return 0;
-	else
-        str++;
-    }
-
-    return 1;
-}
-
-}
-int main(int argc, char *argv[]) {
-    int sum = 0;
-
-    if (argc == 1) {
         printf("0\n");
-        return 0;
+        return (0);
     }
 
-    for (int i = 1; i < argc; i++)
+    for (i = 1; i < argc; i++)
     {
-        if (check_positive_number(argv[i])) {
-            sum += _atoi(argv[i]);
-        } else {
-            printf("Error\n");
-            return 1;
+        for (j = 0; argv[i][j] != '\0'; j++)
+        {
+            if (argv[i][j] < '0' || argv[i][j] > '9')
+            {
+                printf("Error\n");
+                return (1);
+            }
         }
+        sum += atoi(argv[i]);
     }
 
     printf("%d\n", sum);
-    return 0;
+    return (0);
 }
