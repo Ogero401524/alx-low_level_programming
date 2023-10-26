8aeeb7c4f93da72edad28bb829308f411bbf58ce
Ogero401524 <amirakraphael@gmail.com>
2023-10-26 11:43:24 -0700

    added stdio.h linrary

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * get_bit - Returns the value of a bit at a given index.
@@ -9,10 +10,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
+	unsigned long int _mask;
+
+	if (index >= sizeof(unsigned int) * 8)
 		return (-1);
 
-	unsigned long int _mask = 1UL << index;
+	_mask = 1UL << index;
 
 	return ((n & _mask) ? 1 : 0);
 }
