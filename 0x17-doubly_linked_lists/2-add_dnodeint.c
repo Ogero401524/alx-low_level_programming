6d959377a8bf5d33afd1567294df120defd08dd8
Ogero401524 <amirakraphael@gmail.com>
2023-12-08 08:19:48 -0800

    debuging forgot null n newNode

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *forceElementfirst = malloc(sizeof(dlistint_t));
 
-	if (forceElementfirst)
+	if (forceElementfirst == NULL)
 		return (NULL);
 
 	forceElementfirst->n = n;
